c10b77b05673c48fc8b0ada6dc82b184e9019d97
Yaoyu Hu <yaoyuh@andrew.cmu.edu>
2021-02-01 13:41:25 -0500

    RC.

--- CommonPython/Filesystem/FileRecorder.py
+++ CommonPython/Filesystem/FileRecorder.py
@@ -22,6 +22,60 @@ def read_string_list(fn):
 
     return lines
 
+def extract_strings(line, expected, delimiter):
+    ss = line.split(delimiter)
+    n = len(ss)
+
+    assert (n == expected ), \
+        "{} strings extracted from {} with delimiter {}. \
+Expected to be {}. ".format(n, line, delimiter, expected)
+
+    return [ s.strip() for s in ss ]
+
+def read_string_list_2D(fn, expCols, delimiter=",", prefix="", flagSeparateCols=False):
+    """
+    fn is the filename.
+    expCols is the expected columns of each line. 
+    delimiter is the separator between strings.
+    If prefix is not empty, then the prefix string will be added to the front of each string.
+    If flagSeparateCols is True, then the strings will be orgnized as separate columns.
+    """
+    
+    assert (int(expCols) > 0), "expCols = {}. ".format(expCols)
+    expCols = int(expCols)
+
+    if ( False == os.path.isfile( fn ) ):
+        raise Exception("%s does not exist." % (fn))
+    
+    strings2D = []
+    n = 0
+
+    with open(fn, "r") as fp:
+        lines = fp.read().splitlines()
+
+        n = len(lines)
+
+        if ( "" == prefix ):
+            for i in range(n):
+                line = extract_strings(lines[i].strip(), expCols, delimiter)
+                strings2D.append( line )
+        else:
+            for i in range(n):
+                line = extract_strings(lines[i].strip(), expCols, delimiter)
+
+                for j in range(expCols):
+                    line[j] = "%s/%s" % ( prefix, line[j] )
+
+                strings2D.append( line )
+
+    if ( n == 0 ):
+        raise Exception("Read {} failed. ".format(fn))
+
+    if ( flagSeparateCols ):
+        return [ [*entry] for entry in zip(*strings2D) ]
+    else:
+        return strings2D
+
 def write_string_list_2_file(fn, s):
     """
     Write a string list s into a file fn.
